6d41d3d6e5ccf788a2b30d5fc8850219a9f27935
Matt Haynie <mrhaynie@live.com>
2016-12-05 12:33:23 -0800

    Improvements

--- CastingEssentials/Modules/Graphics.cpp
+++ CastingEssentials/Modules/Graphics.cpp
@@ -108,169 +108,13 @@ void CGlowObjectManager::GlowObjectDefinition_t::DrawModel()
 		}
 	}
 }
-static void SetRenderTargetAndViewPort(ITexture *rt, int w, int h)
-{
-	CMatRenderContextPtr pRenderContext(materials);
-	pRenderContext->SetRenderTarget(rt);
-	pRenderContext->Viewport(0, 0, w, h);
-}
-
-enum class GlowMode
-{
-	Outline,	// This is the void, where outlines should be drawn
-	Model,		// Glow model should be drawn in these pixels
-				//Mask,		// Don't draw the model here
-
-				Bits = 1,
-};
-
-class PushRenderTarget final
-{
-public:
-	PushRenderTarget(IMatRenderContext* pRenderContext, ITexture* texture)
-	{
-		m_RenderContext = pRenderContext;
-		m_RenderContext->PushRenderTargetAndViewport();
-		m_RenderContext->SetRenderTargetEx(0, texture);
-	}
-	PushRenderTarget(IMatRenderContext* pRenderContext, ITexture* texture0, ITexture* texture1) : PushRenderTarget(pRenderContext, texture0)
-	{
-		m_RenderContext->SetRenderTargetEx(1, texture1);
-	}
-	~PushRenderTarget()
-	{
-		m_RenderContext->PopRenderTargetAndViewport();
-	}
-
-private:
-	IMatRenderContext* m_RenderContext;
-};
-
-static void SetRenderTargets(IMatRenderContext* renderContext, ITexture* rt0, ITexture* rt1 = nullptr, ITexture* rt2 = nullptr, ITexture* rt3 = nullptr)
-{
-	renderContext->SetRenderTargetEx(0, rt0);
-	renderContext->SetRenderTargetEx(1, rt1);
-	renderContext->SetRenderTargetEx(2, rt2);
-	renderContext->SetRenderTargetEx(3, rt3);
-}
-
-enum RenderTargetType_t
-{
-	NO_RENDER_TARGET = 0,
-	// GR - using shared depth buffer
-	RENDER_TARGET = 1,
-	// GR - using own depth buffer
-	RENDER_TARGET_WITH_DEPTH = 2,
-	// GR - no depth buffer
-	RENDER_TARGET_NO_DEPTH = 3,
-	// only cares about depth buffer
-	RENDER_TARGET_ONLY_DEPTH = 4,
-};
-abstract_class ITextureInternal : public ITexture
-{
-public:
-
-	virtual void Bind(Sampler_t sampler) = 0;
-	virtual void Bind(Sampler_t sampler1, int nFrame, Sampler_t sampler2 = (Sampler_t)-1) = 0;
-
-	// Methods associated with reference counting
-	virtual int GetReferenceCount() = 0;
-
-	virtual void GetReflectivity(Vector& reflectivity) = 0;
-
-	// Set this as the render target, return false for failure
-	virtual bool SetRenderTarget(int nRenderTargetID) = 0;
-
-	// Releases the texture's hw memory
-	virtual void Release() = 0;
-
-	// Called before Download() on restore. Gives render targets a change to change whether or
-	// not they force themselves to have a separate depth buffer due to AA.
-	virtual void OnRestore() = 0;
-
-	// Resets the texture's filtering and clamping mode
-	virtual void SetFilteringAndClampingMode() = 0;
-
-	// Used by tools.... loads up the non-fallback information about the texture 
-	virtual void Precache() = 0;
-
-	// Stretch blit the framebuffer into this texture.
-	virtual void CopyFrameBufferToMe(int nRenderTargetID = 0, Rect_t *pSrcRect = NULL, Rect_t *pDstRect = NULL) = 0;
-	virtual void CopyMeToFrameBuffer(int nRenderTargetID = 0, Rect_t *pSrcRect = NULL, Rect_t *pDstRect = NULL) = 0;
-
-	virtual ITexture *GetEmbeddedTexture(int nIndex) = 0;
-
-	// Get the shaderapi texture handle associated w/ a particular frame
-	virtual ShaderAPITextureHandle_t GetTextureHandle(int nFrame, int nTextureChannel = 0) = 0;
-
-	virtual ~ITextureInternal()
-	{ }
-
-	virtual ImageFormat GetImageFormat() const = 0;
-
-	// Creates a new texture
-	static ITextureInternal *CreateFileTexture(const char *pFileName, const char *pTextureGroupName);
-
-	static ITextureInternal *CreateProceduralTexture(
-		const char			*pTextureName,
-		const char			*pTextureGroupName,
-		int					w,
-		int					h,
-		int					d,
-		ImageFormat			fmt,
-		int					nFlags);
-
-	static ITextureInternal *CreateRenderTarget(
-		const char *pRTName, // NULL for an auto-generated name.
-		int w,
-		int h,
-		RenderTargetSizeMode_t sizeMode,
-		ImageFormat fmt,
-		RenderTargetType_t type,
-		unsigned int textureFlags,
-		unsigned int renderTargetFlags);
-
-	static void ChangeRenderTarget(
-		ITextureInternal *pTexture,
-		int w,
-		int h,
-		RenderTargetSizeMode_t sizeMode,
-		ImageFormat fmt,
-		RenderTargetType_t type,
-		unsigned int textureFlags,
-		unsigned int renderTargetFlags);
-
-	static ITextureInternal *CreateReferenceTextureFromHandle(
-		const char *pTextureName,
-		const char *pTextureGroupName,
-		ShaderAPITextureHandle_t hTexture);
-
-	static void Destroy(ITextureInternal *pTexture);
-
-	// Set this as the render target, return false for failure
-	virtual bool SetRenderTarget(int nRenderTargetID, ITexture* pDepthTexture) = 0;
-
-	// Bind this to a vertex texture sampler
-	virtual void BindVertexTexture(VertexTextureSampler_t sampler, int frameNum = 0) = 0;
-
-	virtual void MarkAsPreloaded(bool bSet) = 0;
-	virtual bool IsPreloaded() const = 0;
-
-	virtual void MarkAsExcluded(bool bSet, int nDimensionsLimit) = 0;
-	virtual bool UpdateExcludedState(void) = 0;
-
-	virtual bool IsTempRenderTarget(void) const = 0;
-
-	// Reload any files the texture is responsible for.
-	virtual void ReloadFilesInList(IFileList *pFilesToReload) = 0;
-};
 
 static void DrawGlowAlways(CUtlVector<CGlowObjectManager::GlowObjectDefinition_t>& glowObjectDefinitions,
 	int nSplitScreenSlot, CMatRenderContextPtr& pRenderContext)
 {
 	ShaderStencilState_t stencilState;
 	stencilState.m_bEnable = true;
-	stencilState.m_nReferenceValue = 1;	// Unique value so we can find this in PIX
+	stencilState.m_nReferenceValue = 1;
 	stencilState.m_CompareFunc = STENCILCOMPARISONFUNCTION_ALWAYS;
 	stencilState.m_PassOp = STENCILOPERATION_REPLACE;
 	stencilState.m_FailOp = STENCILOPERATION_KEEP;
@@ -297,7 +141,7 @@ static ConVar glow_outline_effect_stencil_mode("glow_outline_effect_stencil_mode
 static void DrawGlowOccluded(CUtlVector<CGlowObjectManager::GlowObjectDefinition_t>& glowObjectDefinitions,
 	int nSplitScreenSlot, CMatRenderContextPtr& pRenderContext)
 {
-#if 0	// Enable this when the TF2 team has added IMatRenderContext::OverrideDepthFunc or similar.
+#if ADDED_OVERRIDE_DEPTH_FUNC	// Enable this when the TF2 team has added IMatRenderContext::OverrideDepthFunc or similar.
 	ShaderStencilState_t stencilState;
 	stencilState.m_bEnable = true;
 	stencilState.m_nReferenceValue = 1;
@@ -365,7 +209,7 @@ static void DrawGlowOccluded(CUtlVector<CGlowObjectManager::GlowObjectDefinition
 	stencilState.m_CompareFunc = STENCILCOMPARISONFUNCTION_NOTEQUAL;
 	stencilState.m_PassOp = STENCILOPERATION_REPLACE;
 	stencilState.m_ZFailOp = STENCILOPERATION_REPLACE;
-	stencilState.m_FailOp = STENCILOPERATION_KEEP;
+	stencilState.m_FailOp = glow_outline_effect_stencil_mode.GetBool() ? STENCILOPERATION_KEEP : STENCILOPERATION_REPLACE;
 	stencilState.SetStencilState(pRenderContext);
 
 	for (int i = 0; i < glowObjectDefinitions.Count(); i++)
@@ -411,8 +255,6 @@ static void DrawGlowVisible(CUtlVector<CGlowObjectManager::GlowObjectDefinition_
 	}
 }
 
-#include <PolyHook.h>
-
 void CGlowObjectManager::RenderGlowModels(const CViewSetup *pSetup, int nSplitScreenSlot, CMatRenderContextPtr &pRenderContext)
 {
 	//==========================================================================================//
@@ -449,13 +291,15 @@ void CGlowObjectManager::RenderGlowModels(const CViewSetup *pSetup, int nSplitSc
 		// Draw "glow always" objects
 		pRenderContext->OverrideColorWriteEnable(true, true);
 		pRenderContext->OverrideAlphaWriteEnable(true, true);
-		DrawGlowAlways(m_GlowObjectDefinitions, nSplitScreenSlot, pRenderContext);
 
 		// Draw "glow when visible" objects
 		DrawGlowVisible(m_GlowObjectDefinitions, nSplitScreenSlot, pRenderContext);
 
 		// Draw "glow when occluded" objects
 		DrawGlowOccluded(m_GlowObjectDefinitions, nSplitScreenSlot, pRenderContext);
+
+		// Draw "glow always" objects
+		DrawGlowAlways(m_GlowObjectDefinitions, nSplitScreenSlot, pRenderContext);
 
 		g_pStudioRender->ForcedMaterialOverride(NULL);
 		render->SetColorModulation(vOrigColor.Base());
@@ -496,22 +340,49 @@ void CGlowObjectManager::RenderGlowModels(const CViewSetup *pSetup, int nSplitSc
 		pRenderContext->GetViewport(nViewportX, nViewportY, nViewportWidth, nViewportHeight);
 
 		// Draw quad
-		pRenderContext->DrawScreenSpaceRectangle(pMatHaloAddToScreen, 0, 0, nViewportWidth, nViewportHeight,
-			0, 0, (nSrcWidth - 1) / 4, (nSrcHeight - 1) / 4,
+		pRenderContext->DrawScreenSpaceRectangle(pMatHaloAddToScreen,
+			0, 0, nViewportWidth, nViewportHeight,
+			0, 0, nSrcWidth / 4 - 1, nSrcHeight / 4 - 1,
 			pRtQuarterSize1->GetActualWidth(),
 			pRtQuarterSize1->GetActualHeight());
 	}
+
+	// Done with stencil rendering.
+	pRenderContext->SetStencilEnable(false);
 
 	// No longer need glow models from _rt_FullFrameFB0, move backbuffer (final glows stenciled out) there
 	pRenderContext->CopyRenderTargetToTexture(pRtFullFrameFB0);
 
 	// Move original contents of the backbuffer from _rt_FullFrameFB1 to the backbuffer
-	pRenderContext->CopyTextureToRenderTargetEx(0, pRtFullFrameFB1, nullptr);
+	{
+#if FIXED_COPY_TEXTURE_TO_RENDER_TARGET	// Coordinates don't seem to be mapped 1:1 properly, screen becomes slightly blurry
+		pRenderContext->CopyTextureToRenderTargetEx(0, pRtFullFrameFB1, nullptr);
+#else
+		IMaterial* const pFullFrameFB1 = materials->FindMaterial("debug/debugfbtexture1", TEXTURE_GROUP_RENDER_TARGET);
+		pFullFrameFB1->AddRef();
+		pRenderContext->Bind(pFullFrameFB1);
+
+		const int nSrcWidth = pSetup->width;
+		const int nSrcHeight = pSetup->height;
+		int nViewportX, nViewportY, nViewportWidth, nViewportHeight;
+		pRenderContext->GetViewport(nViewportX, nViewportY, nViewportWidth, nViewportHeight);
+
+		pRenderContext->OverrideDepthEnable(true, false);
+		{
+			pRenderContext->DrawScreenSpaceRectangle(pFullFrameFB1,
+				0, 0, nViewportWidth, nViewportHeight,
+				0, 0, nSrcWidth - 1, nSrcHeight - 1,
+				pRtFullFrameFB1->GetActualWidth(), pRtFullFrameFB1->GetActualHeight());
+		}
+		pRenderContext->OverrideDepthEnable(false, false);
+
+		pFullFrameFB1->Release();
+#endif
+	}
 
 	// Let CGlowObjectManager::ApplyEntityGlowEffects draw final glows from _rt_FullFrameFB0 to the backbuffer,
 	// then we're all done!
 
-	pRenderContext->SetStencilEnable(false);
 	pRenderContext->PopRenderTargetAndViewport();
 }
 
@@ -531,23 +402,18 @@ void CGlowObjectManager::ApplyEntityGlowEffects(const CViewSetup * pSetup, int n
 	int nViewportX, nViewportY, nViewportWidth, nViewportHeight;
 	pRenderContext->GetViewport(nViewportX, nViewportY, nViewportWidth, nViewportHeight);
 
-	{
-		//ITexture* const pRtQuarterSize1 = materials->FindTexture("_rt_SmallFB1", TEXTURE_GROUP_RENDER_TARGET);
-		//IMaterial* const pMatHaloAddToScreen = materials->FindMaterial("dev/halo_add_to_screen", TEXTURE_GROUP_OTHER, true);
-		ITexture* const pRtFullFrameFB1 = materials->FindTexture("_rt_FullFrameFB1", TEXTURE_GROUP_RENDER_TARGET);
-		IMaterial* const pGlowCopy = materials->FindMaterial("dev/glow_copy", TEXTURE_GROUP_OTHER);
+	ITexture* const pRtFullFrameFB0 = materials->FindTexture("_rt_FullFrameFB", TEXTURE_GROUP_RENDER_TARGET);
+	IMaterial* const pGlowCopy = materials->FindMaterial("dev/glow_copy", TEXTURE_GROUP_OTHER);
 
-		if (!IsErrorMaterial(pGlowCopy))
-		{
-			pRenderContext->SetStencilEnable(false);
-			pGlowCopy->AddRef();
-			pRenderContext->Bind(pGlowCopy);
-			pRenderContext->DrawScreenSpaceRectangle(pGlowCopy, 0, 0, nViewportWidth, nViewportHeight,
-				0, 0, nSrcWidth, nSrcHeight,
-				pRtFullFrameFB1->GetActualWidth(),
-				pRtFullFrameFB1->GetActualHeight());
-			pGlowCopy->Release();
-		}
-		pRenderContext->SetStencilEnable(false);
+	if (!IsErrorMaterial(pGlowCopy))
+	{
+		pGlowCopy->AddRef();
+		pRenderContext->Bind(pGlowCopy);
+		pRenderContext->DrawScreenSpaceRectangle(pGlowCopy,
+			0, 0, nViewportWidth, nViewportHeight,
+			0, 0, nSrcWidth - 1, nSrcHeight - 1,
+			pRtFullFrameFB0->GetActualWidth(),
+			pRtFullFrameFB0->GetActualHeight());
+		pGlowCopy->Release();
 	}
 }
