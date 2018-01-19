8f112bdc2223ac2ebb13b637ec09d257f4a7d67c
Matt Haynie <mrhaynie@live.com>
2018-01-19 12:34:41 -0800

    Attempting to fit 2013 vgui_controls to 2018 TF2.

--- CastingEssentials/Misc/OffsetChecking.cpp
+++ CastingEssentials/Misc/OffsetChecking.cpp
@@ -1,4 +1,6 @@
 #include <client/c_baseanimating.h>
+#include <vgui_controls/ImagePanel.h>
+#include <vgui_controls/ProgressBar.h>
 
 #define OFFSET_CHECK(className, memberName, expectedOffset)														\
 	static_assert(!(offsetof(className, memberName) > expectedOffset), "Actual offset greater than expected!");	\
@@ -12,5 +14,36 @@ class OffsetChecking
 	OFFSET_CHECK(C_BaseAnimating, m_bDynamicModelPending, 2185);
 	OFFSET_CHECK(C_BaseAnimating, m_pStudioHdr, 2192);
 
+	OFFSET_CHECK(vgui::ContinuousProgressBar, _unknown0, 388);
+	OFFSET_CHECK(vgui::ContinuousProgressBar, _unknown1, 392);
+	OFFSET_CHECK(vgui::ContinuousProgressBar, _unknown2, 396);
+
 	OFFSET_CHECK(studiohdr_t, numbones, 156);
+
+	OFFSET_CHECK(vgui::PanelMessageMap, baseMap, 24);
+
+	OFFSET_CHECK(vgui::Panel, m_pDragDrop, 44);
+	OFFSET_CHECK(vgui::Panel, m_bToolTipOverridden, 60);
+	OFFSET_CHECK(vgui::Panel, _panelName, 84);
+	OFFSET_CHECK(vgui::Panel, _border, 88);
+	OFFSET_CHECK(vgui::Panel, _flags, 92);
+	OFFSET_CHECK(vgui::Panel, _cursor, 156);
+	OFFSET_CHECK(vgui::Panel, _buildModeFlags, 160);
+	OFFSET_CHECK(vgui::Panel, m_flAlpha, 296);
+	OFFSET_CHECK(vgui::Panel, m_nPaintBackgroundType, 304);
+	OFFSET_CHECK(vgui::Panel, m_nBgTextureId1, 312);
+	OFFSET_CHECK(vgui::Panel, m_nBgTextureId2, 320);
+	OFFSET_CHECK(vgui::Panel, m_nBgTextureId3, 328);
+	OFFSET_CHECK(vgui::Panel, m_nBgTextureId4, 336);
+	OFFSET_CHECK(vgui::Panel, m_roundedCorners, 340);
+
+	OFFSET_CHECK(vgui::ImagePanel, m_pImage, 348);
+	OFFSET_CHECK(vgui::ImagePanel, m_pszImageName, 352);
+
+	OFFSET_CHECK(vgui::ProgressBar, m_iProgressDirection, 352);
+	OFFSET_CHECK(vgui::ProgressBar, _progress, 356);
+	OFFSET_CHECK(vgui::ProgressBar, _segmentGap, 364);
+	OFFSET_CHECK(vgui::ProgressBar, _segmentWide, 368);
+	OFFSET_CHECK(vgui::ProgressBar, m_iBarInset, 372);
+	OFFSET_CHECK(vgui::ProgressBar, m_iBarMargin, 376);
 };
